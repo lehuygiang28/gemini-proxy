6a5ff7089a9ee9121bb841a36a0190072b19a8de
lehuygiang28 <lehuygiang28@gmail.com>
2025-09-28 17:40:49 +0700

    feat(web/data-provider): ✨ implement custom method in Supabase data provider and enhance Supabase client type safety
    This commit introduces a custom method in the Supabase data provider to handle specific requests, logging parameters for debugging purposes. Additionally, it updates the Supabase client configuration to utilize a more specific type for better type safety, ensuring compatibility with the database schema.

--- apps/web/src/providers/data-provider/index.ts
+++ apps/web/src/providers/data-provider/index.ts
@@ -1,6 +1,15 @@
 'use client';
 
+import { type CustomParams } from '@refinedev/core';
 import { dataProvider as dataProviderSupabase } from '@refinedev/supabase';
 import { supabaseBrowserClient } from '@utils/supabase/client';
 
-export const dataProvider = dataProviderSupabase(supabaseBrowserClient);
+const supabaseDP = dataProviderSupabase(supabaseBrowserClient);
+
+export const dataProvider: typeof supabaseDP = {
+    ...supabaseDP,
+    custom: ({ url, method, sorters, filters, payload, query, headers, meta }: CustomParams) => {
+        console.log({ url, method, sorters, filters, payload, query, headers, meta });
+        throw Error('Not implemented on refine-supabase data provider.');
+    },
+};
--- apps/web/src/utils/supabase/client.ts
+++ apps/web/src/utils/supabase/client.ts
@@ -1,8 +1,13 @@
 import { createBrowserClient } from '@supabase/ssr';
+import type { Database } from '@gemini-proxy/database';
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from './constants';
 
-export const supabaseBrowserClient = createBrowserClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-    db: {
-        schema: 'public',
+export const supabaseBrowserClient = createBrowserClient<Database>(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY,
+    {
+        db: {
+            schema: 'public',
+        },
     },
-});
+);
